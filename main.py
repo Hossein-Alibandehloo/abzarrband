7b3edc23e268e53983c352e032c9a8934f1fa04a
hosein alibandehloo <76466142+hosein6222@users.noreply.github.com>
2022-07-08 19:34:16 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
+from __future__ import print_function
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
 import requests
+import pandas as pd
+from time import sleep
 from lxml import html
-wcapi = API(
-    url="https://abzarbrand.com",
-    consumer_key="ck_bb0f947f73f3d690eb816a960703d84e0ad59723",
-    consumer_secret="cs_ad0ce66b5a121e14d7795149425b59f678f4dc98",
-    version="wc/v3"
-)
+
+
 class updater:
-    
-    # def __init__(self, x):
-    #     self.x = x        
+    SCOPES = None
+    creds = None
+    sheet_id_target = None
+    sheet = None
+
+
+    def __init__(self):
+        self.SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+        SERVICE_ACCOUNT_FILE = 'endless-fire.json'
+
+        self.creds = None
+        self.creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=self.SCOPES)
+
+        self.sheet_id_target = '1HHGVnpIxfGZz-icG9Uom-iWUsCFUlMMh_Kw5jIIktpw'
+        service = build('sheets', 'v4', credentials=self.creds)
+        self.sheet = service.spreadsheets()
+ 
     def torob_data(self, url):
       
         req = requests.get(url)
@@ -35,6 +50,7 @@ class updater:
         return {'sellers': sellers, 'price list': price_ls}
     
     def final_price(self, buy_price, price_list):
+        buy_price = int(buy_price)
         for i in range(len(price_list)):
             if buy_price < price_list[0]:
                 plc = 1
@@ -47,3 +63,45 @@ class updater:
             return price_list[plc - 1] - 1000
         else:
             return buy_price + 150000
+    def post_prices(self, startRow, lastRow):
+        df = self.get_data()
+        batch = {'update':[]}
+        for i in range(startRow, lastRow):
+            torob_data = self.torob_data(df['torob link'][i])
+            buy_price = df['buy price'][i]
+            if len(buy_price) > 0:
+                final_price = self.final_price(buy_price, torob_data['price list'])
+                batch['update'].append(
+                    {
+                        'id':df['id'][i],
+                        'sale_price':'',
+                        'regular_price': final_price,
+                        'stock_status': 'instock'
+                    }
+                )
+            else:
+                batch['update'].append(
+                    {
+                        'id':df['id'][i],
+                        'sale_price':'',
+                        'stock_status': 'outofstock'
+                    }
+                )
+        print(wcapi.post("products/batch", batch).json()) 
+        return batch
+    def get_data(self):
+        result = self.sheet.values().get(spreadsheetId=self.sheet_id_target, range= "data!A1:E200").execute()
+        data = result['values']
+        
+        for l in data:
+            max = len(data[0])
+            if len(l) < max:
+                while True:
+                    l.append('')
+                    if len(l) == len(data[0]):
+                        break
+#         index = [first[0] for first in data][1:]
+        headless_data = data[1:]
+        df = pd.DataFrame(headless_data, columns=data[0])
+        
+        return df 
